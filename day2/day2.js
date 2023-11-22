48885ec889658b3ac383cf7a8756c712efc20791
Giramata-douce <giramatadouce69@gmail.com>
2023-11-22 03:11:48 -0800

    update

---
+++ day2/day2.js
@@ -0,0 +1,9 @@
+/*  Jschallenger consolo.log */
+console.log(1);
+// we can assign a value to variable with assignment operator =//
+
+let test =1;
+console.log(test);
+
+let num =0;
+console.log(num);
