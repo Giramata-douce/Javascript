834d09dcb2ccd0f0bdc2e117c81ed4297a7d3ec4
Giramata-douce <giramatadouce69@gmail.com>
2023-11-22 02:13:24 -0800

    my codes of javascript

---
+++ day1/day1.js
@@ -0,0 +1,12 @@
+/* variable numbers is number are named value and store any type of javascript value*/
+var A = 10;
+var B = 20;
+var C = A + B
+console.log("C");
+/* module is positive number */
+let numa =200;
+if (numa%2==0) {
+    console.log("even number");
+}else{
+    console.log("is not even number");
+}
--- modul.js
+++
@@ -1,6 +0,0 @@
-let numa =200;
-if (numa%2==0) {
-    console.log("even number");
-}else{
-    console.log("is not even number");
-}
