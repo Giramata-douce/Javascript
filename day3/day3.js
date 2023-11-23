c08c72aa2e6bc6aabbf342fb3bcc7dd2dd00f9cd
Giramata-douce <giramatadouce69@gmail.com>
2023-11-23 06:26:39 -0800

    variable and boolean

---
+++ day3/day3.js
@@ -0,0 +1,23 @@
+/* variables  */
+let numone =5;
+let numtwo =5;
+console.log(numtwo);
+
+// create variable with const//
+const numa =6;
+console.log(numa);
+const numb = 3;
+console.log('the value of numb is:' +numb);
+let numc = 2;
+numd = 6;
+console.log(numc);
+ const a=2;
+ a =6;
+ console.log(a);
+ /* Booleans */
+
+ let isonline = true;
+ console.log(isonline);
+ isonline = false;
+ console.log(isonline);
+ 
