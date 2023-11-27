f305bc8ed60a4f4ea66b8ad910d5f17238a80782
Giramata-douce <giramatadouce69@gmail.com>
2023-11-27 06:27:18 -0800

    functions

---
+++ day4/day4.js
@@ -0,0 +1,16 @@
+var a = "douce";
+var b = "giramata";
+console.log(a+b);
+let height = 180;
+let anotherheight = height;
+let weight;
+console.log(height);
+console.log(anotherheight);
+weight = 70;
+console.log(weight);
+let stpes = 100;
+console.log(stpes);
+stpes = 120;
+console.log(stpes);
+stpes = stpes + 200;
+console.log(stpes);
