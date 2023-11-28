83bbf1dae63361eac7cbc203b8f5a28ae93ff59c
Giramata-douce <giramatadouce69@gmail.com>
2023-11-28 05:22:22 -0800

    variables

---
+++ day5/day5.js
@@ -0,0 +1,16 @@
+let unitrose =8;
+let untlily =10;
+let untitulp =2;
+// unit price//
+let numrose =70;
+let numlily =50;
+let numtulp =120;
+// quantities//
+let totalrose = unitrose*numrose;
+let totallily = untlily*numlily;
+let totaltulp = untitulp*numtulp;
+let total = totalrose + totallily + totaltulp;
+console.log("rose-unit price:",unitrose,",quantity:",numrose,",value:",totalrose);
+console.log("lily-unit price:",untlily,",quantity:",numlily,",value:",totallily);
+console.log("tulp-unit price:",untitulp,",quantity:",numtulp,",value:",totaltulp);
+
