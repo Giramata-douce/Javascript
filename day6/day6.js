32c8e250b54f90c766ae7cb84283294109e13766
Giramata-douce <giramatadouce69@gmail.com>
2023-11-29 06:46:27 -0800

    datatypes primitives.

---
+++ day6/day6.js
@@ -0,0 +1,47 @@
+/* datatypes */
+let year =1990;
+console.log(typeof year);
+console.log(typeof 1990);
+let name = "Alice";
+console.log(typeof name);
+console.log(typeof "Alice");
+let typeOfyear = typeof year;
+console.log(typeOfyear);
+console.log(typeof typeOfyear);
+
+// primitive datatype Boolean//
+let isDatavalid = true;
+let isStringtoolong = false;
+let isGameOver = false;
+continueLoop = true;
+console.log(false);
+console.log(typeof false);
+console.log(isDatavalid);
+console.log(typeof isDatavalid);
+// numbers//
+let a = 10;
+let b = 0x10;
+let c = 0o10;
+let d = 0b10;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+let x = 9e3;
+let y = 123e-5;
+console.log(x);
+console.log(y);
+// string//
+let country = "Rwanda";
+let conent = "Africa";
+console.log(country);
+console.log(typeof country);
+console.log(conent);
+console.log(typeof conent);
+
+let message1 = "Hello my name is 'Douce'.";
+let message2 = 'From Kigali "Gisozi".';
+console.log(message1);
+console.log(message2);
+
+
