5ff3bf32e3a712f98ea17a832064ae40a25be0a1
Giramata-douce <giramatadouce69@gmail.com>
2023-11-30 10:26:27 -0800

    primitives

---
+++ day7/day7.js
@@ -0,0 +1,18 @@
+ let  path  =  "C:\\"  +  "Windows";
+ console.log(path);  
+  
+let  test  =  "100"  +  "10";
+ console.log(test);  
+console.log(typeof  test); 
+ let  path1  =  "C:\\Windows"  -  "Windows";
+ console.log(path);
+  
+ let  test1  =  "100"  -  "10";
+ console.log(test);  
+ console.log(typeof  test);  
+ // string interpolation//
+  let  country  =  "Malawi";
+ let  continent  =  "Africa";
+   
+ let  sentence  =  `  ${country}  is  located  in  ${continent}.`;
+ console.log(sentence);  
